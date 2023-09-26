358b12f7ae9131007fdc74b064c8be32d0a716b6
Chrisdewan2 <146055332+Chrisdewan2@users.noreply.github.com>
2023-09-26 11:50:03 +0700

    Add files via upload

---
+++ Polynomial Equations.c
@@ -0,0 +1,15 @@
+#define PI 3.1416
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a=1, b=0,c=-4,x;
+    x=(-b+(sqrt(b*b-4*a*c))/(2*a);   printf("x= %f\n", x);
+    a=1;b=0;c=-4;
+    x=(-b-(sqrt(b*b-4*a*c))/(2*a);   printf("x= %f\n", x);
+
+
+    return 0;
+}
